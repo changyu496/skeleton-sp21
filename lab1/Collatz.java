4ed2d39ea5e2dfc3a56e5065d453139512bb59bf
changyu <changyu496@gmail.com>
2024-01-20 11:24:22 +0800

    [commit]第一个lab，完成nextNumber方法

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,26 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ *
+ * @author YOUR NAME HERE
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+
+    /**
+     * 如果是奇数 返回 3*n+1
+     * 如果是偶数 返回 n/2
+     * 如果是1 返回仍是1
+     *
+     * @param n 需要计算的数据
+     * @return
+     */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        } else if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
